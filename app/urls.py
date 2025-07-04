54958ba07be5cac964c062e019695dd5f0895075
arturfew <arturfelipesouza64@gmail.com>
2025-07-04 17:54:45 -0300

    dinamico

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='inicio'),# rota para "/
+    path('', views.inicio, name='inicio'),
+    path('jogadores/', views.jogadores, name='jogadores'),
     path('sobre/', views.sobre, name='sobre'),
-    path('jogadores/', views.jogadores, name='jogadores'),  
 ]
--- app/views.py
+++ app/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home(request):
+def inicio(request):
     return render(request, "inicio.html")
 
-def jogadores(request):
+def jogadores(request): 
     user_list = [
-        {"nome": "João Pedro", "matricula": "0001", "idade": 17, "cidade": "Riachuelo"},
-        {"nome": "Hudson", "matricula": "0002", "idade": 17, "cidade": "Boa Saúde"},
-        {"nome": "joão filho", "matricula": "0003", "idade": 18, "cidade": "Serra Caiada"},
+        {"nome": "Jole Pedro", "matricula": "0003", "idade": 17, "cidade": "Riachuelo"},
+        {"nome": "Nudson", "matricula": "0002", "idade": 17, "cidade": "Boa Saúde"},
+        {"nome": "Jole Filho", "matricula": "0003", "idade": 18, "cidade": "Sarra Cidade"},
         {"nome": "Artur", "matricula": "0004", "idade": 17, "cidade": "Santa Maria"},
-        {"nome": "Murilo", "matricula": "0005", "idade": 17, "cidade": "são tome"},
+        {"nome": "Murillo", "matricula": "0005", "idade": 17, "cidade": "São Tomé"},
     ]
-            
     context = {
-        "jogadores": user_list,
+        "jogadores": user_list,  
     }
     return render(request, "jogadores.html", context)
-            
 
-def sobre(request):
+def sobre(request):  
     return render(request, "sobre.html")
