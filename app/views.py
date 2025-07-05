3a9d65f8f95be72ec18cde03fca514298bd0f371
igor murilo <igormurilo.ac.21@gmal.com>
2025-07-05 13:23:16 -0300

    elenco e for feitos

--- app/views.py
+++ app/views.py
@@ -5,12 +5,19 @@ def inicio(request):
 
 def jogadores(request): 
     user_list = [
-        {"nome": "Jole Pedro", "matricula": "0003", "idade": 17, "cidade": "Riachuelo"},
-        {"nome": "Nudson", "matricula": "0002", "idade": 17, "cidade": "Boa Saúde"},
-        {"nome": "Jole Filho", "matricula": "0003", "idade": 18, "cidade": "Sarra Cidade"},
-        {"nome": "Artur", "matricula": "0004", "idade": 17, "cidade": "Santa Maria"},
-        {"nome": "Murillo", "matricula": "0005", "idade": 17, "cidade": "São Tomé"},
-    ]
+  {"nome": "Diego Alves", "posicao": "goleiro", "idade": 34, "nascimento": "Rio de Janeiro"},
+  {"nome": "Rafinha", "posicao": "lateral-direito", "idade": 34, "nascimento": "Londrina"},
+  {"nome": "Rodrigo Caio", "posicao": "zagueiro", "idade": 26, "nascimento": "Dracena"},
+  {"nome": "Pablo Marí", "posicao": "zagueiro", "idade": 26, "nascimento": "Valência"},
+  {"nome": "Filipe Luís", "posicao": "lateral-esquerdo", "idade": 34, "nascimento": "Jaraguá do Sul"},
+  {"nome": "Willian Arão", "posicao": "volante", "idade": 27, "nascimento": "São Paulo"},
+  {"nome": "Gerson", "posicao": "meia", "idade": 22, "nascimento": "Rio de Janeiro"},
+  {"nome": "Éverton Ribeiro", "posicao": "meia", "idade": 30, "nascimento": "Arujá"},
+  {"nome": "Arrascaeta", "posicao": "meia", "idade": 25, "nascimento": "Nuevo Berlín"},
+  {"nome": "Bruno Henrique", "posicao": "atacante", "idade": 28, "nascimento": "Belo Horizonte"},
+  {"nome": "Gabigol", "posicao": "atacante", "idade": 23, "nascimento": "São Bernardo do Campo"}
+]
+
     context = {
         "jogadores": user_list,  
     }
