3f226aa5f10be6630aa127f18a3273e4c666f732
igor murilo <igormurilo.ac.21@gmal.com>
2025-09-17 23:15:51 -0300

    falta apenas colocar os jogadores no /admin

--- app/views.py
+++ app/views.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post 
+
+def jogadores(request):
+    posts = Post.objects.all().order_by('data_publicacao')  # pega todos os posts
+    return render(request, 'app/jogadores.html', {'posts': posts})
+
 
 def inicio(request):
-    return render(request, "inicio.html")
+    return render(request, "app/inicio.html")
 
-def jogadores(request): 
-    user_list = [
-  {"nome": "Diego Alves", "posicao": "goleiro", "idade": 34, "nascimento": "Rio de Janeiro", "foto": "img/foto-diego-alves.jpg" },
-  {"nome": "Rafinha", "posicao": "lateral-direito", "idade": 34, "nascimento": "Londrina" , "foto": "img/foto-rafinha.jpg"},
-  {"nome": "Rodrigo Caio", "posicao": "zagueiro", "idade": 26, "nascimento": "Dracena" , "foto": "img/foto-rodrigo-caio.png"},
-  {"nome": "Pablo Marí", "posicao": "zagueiro", "idade": 26, "nascimento": "Valência" , "foto": "img/foto-pablo-mari.jpg"},
-  {"nome": "Filipe Luís", "posicao": "lateral-esquerdo", "idade": 34, "nascimento": "Jaraguá do Sul", "foto": "img/foto-felipe-luis.png"},
-  {"nome": "Willian Arão", "posicao": "volante", "idade": 27, "nascimento": "São Paulo" , "foto": "img/foto-arao.jpg"},
-  {"nome": "Gerson", "posicao": "meia", "idade": 22, "nascimento": "Rio de Janeiro" , "foto": "img/foto-gerson.jpg"},
-  {"nome": "Éverton Ribeiro", "posicao": "meia", "idade": 30, "nascimento": "Arujá" , "foto": "img/foto-everton-ribeiro.jpg"},
-  {"nome": "Arrascaeta", "posicao": "meia", "idade": 25, "nascimento": "Nuevo Berlín" , "foto": "img/foto-arrascaeta.jpg"},
-  {"nome": "Bruno Henrique", "posicao": "atacante", "idade": 28, "nascimento": "Belo Horizonte" , "foto": "img/foto-bruno-henrique.jpg"},
-  {"nome": "Gabigol", "posicao": "atacante", "idade": 23, "nascimento": "São Bernardo do Campo",   "foto": "img/foto-gabigol.jpg"},
-  {"nome": "Jorge Jesus", "posicao": "tecnico", "idade": 70, "nascimento": "Amadora, Portugal",   "foto": "img/foto-jorge-jesus.png"},
-  
-]
 
-    context = {
-        "jogadores": user_list,  
-    }
-    return render(request, "jogadores.html", context)
 
 def sobre(request): 
     titulos_list = [
@@ -36,7 +22,7 @@ def sobre(request):
     context = {
         "sobre": titulos_list,
     }
-    return render(request, "sobre.html", context)
+    return render(request, "app/sobre.html", context)
     
 
 
--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,21 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 # Esta linha renderiza o template de detalhe, passando o post específico
     return render(request, 'blog/post_detail.html', {'post': post})
+
+# blog/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+# Esta é a função que estava faltando!
+def post_list(request):
+    # Esta linha busca todos os objetos Post no banco de dados, ordenando pelos mais recentes
+    posts = Post.objects.all().order_by('-data_publicacao') 
+    # Esta linha renderiza o template HTML, passando a lista de posts para ele
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Esta função será usada pela outra URL, para mostrar um post individual
+def post_detail(request, pk):
+    # Esta linha busca o post com o ID (pk) específico, ou mostra um erro 404 se não encontrar
+    post = get_object_or_404(Post, pk=pk) 
+    # Esta linha renderiza o template de detalhe, passando o post específico
+    return render(request, 'blog/post_detail.html', {'post': post})
